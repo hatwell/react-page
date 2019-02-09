0a381d11af7078a6cef0593e4bb24239abe69bd1
Caroline Hatwell <caroline.hatwell@coopdigital.co.uk>
2019-02-09 11:41:53 +0000

    content change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> dragon pig
+            i am a dragon pig
           </p>
           <a
             className="App-link"
